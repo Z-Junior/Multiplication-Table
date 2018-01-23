bc4f360d012e2a23a9d7640d30fc872368824b89
RumbleFrog <xxlosttechxx@gmail.com>
2018-01-23 09:20:35 -0500

    MT

---
+++ src/MT.java
@@ -0,0 +1,26 @@
+public class MT
+{
+    public static void main(String[] args)
+    {
+        int[][] iTable = new int[12][12];
+
+        for (int i = 1; i < iTable.length; i++)
+            for (int j = 1; j < iTable[i].length; j++)
+                iTable[i][j] = i*j;
+
+        printTable(iTable);
+    }
+
+    static void printTable(int[][] iTable)
+    {
+        for (int i = 1; i < iTable.length; i++)
+        {
+            for (int j = 1; j < iTable[i].length; j++)
+            {
+                System.out.print(String.format("%d\t", iTable[i][j]));
+            }
+
+            System.out.println();
+        }
+    }
+}
